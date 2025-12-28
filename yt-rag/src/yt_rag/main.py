4ec590bdbae8260aa1db88c66184634809d9e78f
AmanKadam-16 <aman16112002kadam@gmail.com>
2025-12-29 01:37:34 +0530

    add: runner

--- yt-rag/src/yt_rag/main.py
+++ yt-rag/src/yt_rag/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 from src.yt_rag.router.api_router import router as master_router
@@ -28,28 +27,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
-=======
-from fastapi.responses import JSONResponse
-from fastapi import FastAPI
-from src.yt_rag.router.api_router import router as master_router
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI(
-    title="Youtube RAG",
-    description="""Capabilities include Question / Answering over youtube video | Transcript and even Visual questioning""",
-)
-app.include_router(master_router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-def root():
-    return JSONResponse(status_code=200, content="System Operational.")
->>>>>>> 30b90cef030d227b40273896501a5d3f776f94ee
